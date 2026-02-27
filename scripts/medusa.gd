1844869aab03ac17a930af1f4e534354891b8066
otk-laptop <loadingweb06@gmail.com>
2026-02-27 22:41:39 +0000

    working on petrify

--- scripts/medusa.gd
+++ scripts/medusa.gd
@@ -1,1 +1,14 @@
 extends Node2D
+
+func petrify():
+	var state_space = get_world_2d().direct_space_state
+	var enemies : Array[Node] = get_tree().current_scene.get_tree().get_nodes_in_group("Enemies")
+	for enemy : Node in enemies:
+		if enemy is Enemy:
+			var query = PhysicsRayQueryParameters2D.create(position, enemy.position)
+			var result = state_space.intersect_ray(query)
+			print(result)
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("Petrify"):
+		petrify()
