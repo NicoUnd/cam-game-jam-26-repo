1fdd44cacce18af27d50092f61d7433cf0cfd896
otk-laptop <loadingweb06@gmail.com>
2026-02-27 22:53:34 +0000

    medusa work

--- scripts/medusa.gd
+++ scripts/medusa.gd
@@ -1,11 +1,14 @@
 extends Node2D
 
+@onready var player : CharacterBody2D = $".."
+
 func petrify():
 	var state_space = get_world_2d().direct_space_state
 	var enemies : Array[Node] = get_tree().current_scene.get_tree().get_nodes_in_group("Enemies")
 	for enemy : Node in enemies:
 		if enemy is Enemy:
 			var query = PhysicsRayQueryParameters2D.create(position, enemy.position)
+			query.exclude = [self, enemy, player]
 			var result = state_space.intersect_ray(query)
 			print(result)
 
