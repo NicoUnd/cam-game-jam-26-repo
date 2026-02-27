6e55a8ea292eb913c68bd120973fdaaaca2a1c2c
knyckk <th702@cam.ac.uk>
2026-02-27 23:57:32 +0000

    extra enemy

--- scripts/enemies/enemy.gd
+++ scripts/enemies/enemy.gd
@@ -2,12 +2,17 @@ extends Node2D
 class_name Enemy
 
 var collision_shape_2d: CollisionShape2D;
+static var player : Player = Player.player
+@export var speed : float = 100
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
+func petrify() -> void:
+	print("dead")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	position = position.move_toward(player.global_position, speed * delta)
