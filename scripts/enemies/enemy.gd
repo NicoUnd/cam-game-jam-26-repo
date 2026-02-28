44a17511fd2427759747a8553d7ab5374ba1ec6d
knyckk <th702@cam.ac.uk>
2026-02-28 00:04:25 +0000

    enemy stuff

--- scripts/enemies/enemy.gd
+++ scripts/enemies/enemy.gd
@@ -2,17 +2,19 @@ extends Node2D
 class_name Enemy
 
 var collision_shape_2d: CollisionShape2D;
-static var player : Player = Player.player
+static var player : Player 
 @export var speed : float = 100
+var _is_petrified : bool = false
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	player = Player.player
 
 func petrify() -> void:
-	print("dead")
+	_is_petrified = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	position = position.move_toward(player.global_position, speed * delta)
+	if not _is_petrified:
+		position = position.move_toward(player.global_position, speed * delta)
