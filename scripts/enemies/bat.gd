7c9c48010bd67d2c35d877e1e523511bddc0e384
NicoUnd <nj.galvez@yahoo.co.uk>
2026-03-01 01:29:20 +0000

    levels 3-5

--- scripts/enemies/bat.gd
+++ scripts/enemies/bat.gd
@@ -10,6 +10,8 @@ func move(delta: float) -> void:
 
 
 func _on_timer_timeout() -> void:
+	if player.state == Player.PLAYER_STATE.DYING:
+		return;
 	match state:
 		ENEMY_STATE.IDLE:
 			linear_velocity = Vector2.RIGHT.rotated(randf_range(0, 2*PI)) * speed/2
