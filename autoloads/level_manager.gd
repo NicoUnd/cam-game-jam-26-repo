f1766c00fe141474453bf3fd01b1edea6e2d63f9
knyckk <th702@cam.ac.uk>
2026-03-01 02:53:58 +0000

    can escape

--- autoloads/level_manager.gd
+++ autoloads/level_manager.gd
@@ -1,3 +1,7 @@
 extends Node
 var current_level_index: int = 0
 var max_level_index: int = 10
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("Escape"):
+		get_tree().change_scene_to_file("res://scenes/ui/main_menu.tscn")
--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -3,7 +3,9 @@ extends Control
 @export var menu_music : AudioStream
 
 func _ready() -> void:
+	AudioManager.music_volume(100)
 	AudioManager.play_music(menu_music)
+	
 
 func start_game():
 	get_tree().change_scene_to_file("res://scenes/ui/level_select.tscn")
