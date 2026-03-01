112f25394f7f362b955a3309065e10eebdf9c623
NicoUnd <nj.galvez@yahoo.co.uk>
2026-03-01 03:41:50 +0000

    level 6 done

--- scripts/muddy_puddle.gd
+++ scripts/muddy_puddle.gd
@@ -1,16 +1,23 @@
 extends Area2D
 
-@export var _speed_multiplier: float = 0.5
+@export var _speed_multiplier: float = 0.4;
+
+var minotaurs: Array[Mintoaur] = [];
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is Player:
 		body.player_speed *= _speed_multiplier
 	elif body is Enemy:
 		body.speed *= _speed_multiplier
-		body.velocity *= 0.2;
+		if body is Mintoaur:
+			minotaurs.append(body);
+			body.in_puddle = true;
 
 func _on_body_exited(body: Node2D) -> void:
 	if body is Player:
 		body.player_speed /= _speed_multiplier
 	elif body is Enemy:
 		body.speed /= _speed_multiplier
+		if body is Mintoaur:
+			body.in_puddle = false;
+			minotaurs.erase(body);
